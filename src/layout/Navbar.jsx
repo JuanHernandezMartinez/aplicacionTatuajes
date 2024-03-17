1ef365e9ac46427397c4040c451ae7f807415559
Juan Hernandez <juan.martinez3168@gmail.com>
2024-03-17 14:32:09 -0600

    Instale la libreria de bootrsap y cambie los botones del navbar pro botones de boostrap

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/Navbar.css'
 import { useNavigate } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 export default function Navbar() {
   const navigate=useNavigate()
@@ -12,10 +13,10 @@ export default function Navbar() {
       
         <nav className='barra-navegacion'>
             <ul>
-                <li onClick={()=>navigate("/inicio")}>Incio</li> 
-                <li onClick={()=>navigate("/Blog")}>Blog</li>
-                <li onClick={()=>navigate("/Contacto")}>Contacto</li>
-                <li onClick={()=>navigate("/Galeria")}>Galeria</li>
+                <Button className='m-2' variant='dark' onClick={()=>navigate("/inicio")}>INICIO</Button>
+                <Button className='m-2' variant='dark' onClick={()=>navigate("/Blog")}>Blog</Button>
+                <Button className='m-2' variant='dark' onClick={()=>navigate("/Contacto")}>Contacto</Button>
+                <Button className='m-2' variant='dark' onClick={()=>navigate("/Galeria")}>Galeria</Button>
 
             </ul>
         </nav>
