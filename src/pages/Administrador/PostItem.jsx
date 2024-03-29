74d5fa9f6c4008604caf019b6d6112b87140bb2f
pinky2203 <manuelayalamontes52@gmail.com>
2024-03-28 22:24:14 -0600

    y la perra seguia y seguia

--- src/pages/Administrador/PostItem.jsx
+++ src/pages/Administrador/PostItem.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../Administrador/Styles_post/Stpost.css'
 
+
 const PostItem = ({ PostID, titulo, desc, imagen }) => {
     return (
         <article className="post">
             <div className="post_imagen">
-                <img src={imagen} alt={titulo} />
+                <img src={imagen} class="rounded " alt={titulo} />
             </div>
             <div className="post_content">
                 <Link to={`/PostHome/${PostID}`}>
-                    <h3>{titulo}</h3>
+                    <h3 className='titulo'>{titulo}</h3>
                 </Link>
                 <p className='Desc'> {desc} </p>
 
